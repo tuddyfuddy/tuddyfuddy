32b632abfad31d054169132fcbab908b94b04968
최영빈 <swybean@gmail.com>
2024-10-28 17:41:31 +0900

    Kotlin Jetpack Compose 기초

--- frontend/app/src/main/java/com/survivalcoding/a510/MainActivity.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/MainActivity.kt
@@ -7,15 +7,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
@@ -27,6 +31,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.survivalcoding.a510.ui.theme.A510Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,16 +57,27 @@ fun MyApp(modifier: Modifier = Modifier,
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false)}
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
-        color= MaterialTheme.colorScheme.primary,
-        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Column(modifier = modifier.padding(24.dp)
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
-            Text(text= "Hello ")
-            Text(text= name)
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value}
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
-
     }
 }
 
