2bc6dab4a28c7cf719a450fc9e0e33ae1e1e1426
김인엽 <inyub98@gmail.com>
2024-10-29 10:38:52 +0900

    :hammer: Chore: add discovery client annotation

--- health-service/src/main/java/com/heejuk/tuddyfuddy/healthservice/HealthServiceApplication.java
+++ health-service/src/main/java/com/heejuk/tuddyfuddy/healthservice/HealthServiceApplication.java
@@ -2,12 +2,14 @@ package com.heejuk.tuddyfuddy.healthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HealthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HealthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HealthServiceApplication.class, args);
+    }
 
 }
