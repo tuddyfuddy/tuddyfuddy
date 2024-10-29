9131ec07cbf1b4d7f6b0c3abd844c3ba3c6b01a0
최영빈 <swybean@gmail.com>
2024-10-29 15:29:04 +0900

    :sparkles: Feat: TopBar 색깔 상태표시줄에 적용

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.accompanist.systemuicontroller)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- frontend/app/src/main/java/com/survivalcoding/a510/components/TopBar.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/components/TopBar.kt
@@ -9,18 +9,32 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.survivalcoding.a510.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colorScheme.surface.luminance() > 0.5
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = Color(0xFF90EE90),
+            darkIcons = useDarkIcons
+        )
+    }
+
     TopAppBar(
         modifier = Modifier.height(100.dp),
         colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFF90EE90)),
