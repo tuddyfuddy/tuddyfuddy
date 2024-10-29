da2b93ee7bce140b3d7bab7cf64369389567fb50
김인엽 <inyub98@gmail.com>
2024-10-29 10:21:46 +0900

    :hammer: Chore: init project

---
+++ health-service/src/test/java/com/heejuk/tuddyfuddy/healthservice/HealthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heejuk.tuddyfuddy.healthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/com/heejuk/tuddyfuddy/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heejuk.tuddyfuddy.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
