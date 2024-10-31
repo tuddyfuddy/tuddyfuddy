8d0fa641c3ed2bffd7eb950c6ad38175c5055415
김인엽 <inyub98@gmail.com>
2024-10-31 14:07:41 +0900

    :sparkles: Feat: logic 분리

--- context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/service/HealthService.java
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/service/HealthService.java
@@ -1,12 +1,17 @@
 package com.heejuk.tuddyfuddy.contextservice.service;
 
-import com.heejuk.tuddyfuddy.contextservice.entity.*;
-import com.heejuk.tuddyfuddy.contextservice.repository.*;
-import java.time.*;
-import java.util.*;
-import lombok.*;
-import lombok.extern.slf4j.*;
-import org.springframework.stereotype.*;
+import com.heejuk.tuddyfuddy.contextservice.detector.HealthAnomalyDetector;
+import com.heejuk.tuddyfuddy.contextservice.dto.request.HealthRequest;
+import com.heejuk.tuddyfuddy.contextservice.dto.response.HealthResponse;
+import com.heejuk.tuddyfuddy.contextservice.entity.Health;
+import com.heejuk.tuddyfuddy.contextservice.mapper.HealthMapper;
+import com.heejuk.tuddyfuddy.contextservice.repository.HealthRepository;
+import com.heejuk.tuddyfuddy.contextservice.util.DateUtil;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
@@ -14,37 +19,34 @@ import org.springframework.stereotype.*;
 public class HealthService {
 
     private final HealthRepository healthRepository;
+    private final HealthMapper healthMapper;
+    private final HealthAnomalyDetector detector;
+    private final DateUtil dateUtil;
 
-    public Health saveHealthData(Health health) {
-        health.setCreatedAt(LocalDateTime.now());
-        health.setUpdatedAt(LocalDateTime.now());
+    public HealthResponse saveHealthData(
+        HealthRequest health,
+        Long userId
+    ) {
+        Health entity = healthMapper.toEntity(health);
 
-        Health savedData = healthRepository.save(health);
-        if (isAbnormalHealthData(savedData)) {
-            log.info("Abnormal health data detected");
-        }
-        return savedData;
+        entity.setUserId(userId);
+
+        entity.setCreatedAt(dateUtil.getCurrentDateTime());
+        entity.setUpdatedAt(dateUtil.getCurrentDateTime());
+
+        Health savedData = healthRepository.save(entity);
+        detector.detectAnomalies(savedData);
+
+        return healthMapper.toDto(savedData);
     }
 
-    /**
-     * 이상 징후 감지
-     *
-     * @param data
-     * @return
-     */
-    private boolean isAbnormalHealthData(Health data) {
-        if (data.getHeartRate() != null && data.getHeartRate() > 200) {
-            log.info("[위험] 심박수가 200 이상입니다.");
-            return true;
-        }
-        return false;
-    }
-
-    public List<Health> getUserHealthData(
+    public List<HealthResponse> getUserHealthData(
         Long userId,
         LocalDateTime start,
         LocalDateTime end
     ) {
-        return healthRepository.findByUserIdAndTimestampBetween(userId, start, end);
+        return healthMapper.toDtoList(healthRepository.findByUserIdAndTimestampBetween(userId,
+                                                                                       start,
+                                                                                       end));
     }
 }
