92bedd90c72ad8a152cbc1b3e13f70762419222c
김인엽 <inyub98@gmail.com>
2024-10-31 15:41:13 +0900

    :sparkles: Feat: jpa datasource 설정 제거, feign 설정추가

--- context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/ContextServiceApplication.java
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/ContextServiceApplication.java
@@ -2,14 +2,21 @@ package com.heejuk.tuddyfuddy.contextservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.*;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ContextServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContextServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContextServiceApplication.class,
+                              args);
+    }
 
 }
