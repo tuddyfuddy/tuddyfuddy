6803880b2c89f8c72112896a95c41f8922b50cf6
김인엽 <inyub98@gmail.com>
2024-10-31 15:41:59 +0900

    :sparkles: Feat: Mongo config 줄이기

--- context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/config/MongoConfig.java
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/config/MongoConfig.java
@@ -1,32 +1,16 @@
 package com.heejuk.tuddyfuddy.contextservice.config;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Arrays;
-import java.util.Date;
-import org.springframework.beans.factory.annotation.Value;
+import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.convert.ReadingConverter;
-import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.heejuk.tuddyfuddy.contextservice.repository")
+@EnableMongoAuditing
 public class MongoConfig {
-
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-
-    @Value("${spring.data.mongodb.port}")
-    private int port;
-
-    @Value("${spring.data.mongodb.database}")
-    private String database;
 
     @Bean
     public MongoTemplate mongoTemplate(MongoDatabaseFactory databaseFactory) {
@@ -34,30 +18,7 @@ public class MongoConfig {
     }
 
     @Bean
-    public MongoCustomConversions mongoCustomConversions() {
-        return new MongoCustomConversions(Arrays.asList(
-            new LocalDateTimeToDateConverter(),
-            new DateToLocalDateTimeConverter()
-        ));
-    }
-
-    @WritingConverter
-    static class LocalDateTimeToDateConverter implements Converter<LocalDateTime, Date> {
-
-        @Override
-        public Date convert(LocalDateTime source) {
-            return Date.from(source.atZone(ZoneId.of("Asia/Seoul"))
-                                   .toInstant());
-        }
-    }
-
-    @ReadingConverter
-    static class DateToLocalDateTimeConverter implements Converter<Date, LocalDateTime> {
-
-        @Override
-        public LocalDateTime convert(Date source) {
-            return LocalDateTime.ofInstant(source.toInstant(),
-                                           ZoneId.of("Asia/Seoul"));
-        }
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("system");
     }
 }
