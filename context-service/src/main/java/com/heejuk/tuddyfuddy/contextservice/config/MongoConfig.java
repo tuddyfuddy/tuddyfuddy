cf63d2dd92f74a0bd8f38e5944a1478cb327c140
김인엽 <inyub98@gmail.com>
2024-10-31 14:08:12 +0900

    :sparkles: Feat: Mongodb config for datetime converter(KR timezone)

--- context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/config/MongoConfig.java
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/config/MongoConfig.java
@@ -1,10 +1,18 @@
 package com.heejuk.tuddyfuddy.contextservice.config;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
@@ -23,5 +31,33 @@ public class MongoConfig {
     @Bean
     public MongoTemplate mongoTemplate(MongoDatabaseFactory databaseFactory) {
         return new MongoTemplate(databaseFactory);
+    }
+
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(Arrays.asList(
+            new LocalDateTimeToDateConverter(),
+            new DateToLocalDateTimeConverter()
+        ));
+    }
+
+    @WritingConverter
+    static class LocalDateTimeToDateConverter implements Converter<LocalDateTime, Date> {
+
+        @Override
+        public Date convert(LocalDateTime source) {
+            return Date.from(source.atZone(ZoneId.of("Asia/Seoul"))
+                                   .toInstant());
+        }
+    }
+
+    @ReadingConverter
+    static class DateToLocalDateTimeConverter implements Converter<Date, LocalDateTime> {
+
+        @Override
+        public LocalDateTime convert(Date source) {
+            return LocalDateTime.ofInstant(source.toInstant(),
+                                           ZoneId.of("Asia/Seoul"));
+        }
     }
 }
