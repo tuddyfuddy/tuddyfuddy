2a6c9b4135edeea8ddaf59bba18d7aa17c17980f
김인엽 <inyub98@gmail.com>
2024-10-31 15:42:48 +0900

    :sparkles: Feat: openfeign 방식으로 교체

---
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/config/FeignErrorDecoder.java
@@ -0,0 +1,37 @@
+package com.heejuk.tuddyfuddy.contextservice.config;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import jakarta.ws.rs.ClientErrorException;
+import jakarta.ws.rs.ServerErrorException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FeignErrorDecoder implements ErrorDecoder {
+
+    @Override
+    public Exception decode(
+        String methodKey,
+        Response response
+    ) {
+        log.error("Error occurred during feign client call: {} {}",
+                  methodKey,
+                  response.status());
+
+        if (response.status() >= 400 && response.status() <= 499) {
+            return new ClientErrorException(
+                "Client error occurred during external service call",
+                response.status()
+            );
+        }
+
+        if (response.status() >= 500 && response.status() <= 599) {
+            return new ServerErrorException(
+                "Server error occurred during external service call",
+                response.status()
+            );
+        }
+
+        return new Exception("Unknown error occurred");
+    }
+}
