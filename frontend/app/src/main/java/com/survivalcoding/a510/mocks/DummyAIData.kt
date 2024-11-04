097535fb0aff7ee09f82d8ce06dc248dd432fa56
최영빈 <swybean@gmail.com>
2024-11-04 21:14:50 +0900

    :recycle: Refactor: DummyAIData 수정 - 초기 인사말

--- frontend/app/src/main/java/com/survivalcoding/a510/mocks/DummyAIData.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/mocks/DummyAIData.kt
@@ -9,17 +9,17 @@ object DummyAIData {
             id = 1,
             profileImage = R.drawable.cha,
             name = "활명수",
-            message = "늦었다고오오오오 생각할 때가 진짜 늦은 거야",
-            timestamp = "2분 전",
-            unreadCount = 2
+            message = "반가워! 너는 이름이 뭐야?",
+            timestamp = "방금 전",
+            unreadCount = 1
         ),
         ChatData(
             id = 2,
             profileImage = R.drawable.back,
             name = "백지헌",
-            message = "갑자기 비내리는거 같은데 ㅠㅠ",
-            timestamp = "오후 2:40",
-            unreadCount = 3
+            message = "하이하이!! 얼른 나랑 놀아줘!!!!",
+            timestamp = "방금 전",
+            unreadCount = 1
         )
 
     )
