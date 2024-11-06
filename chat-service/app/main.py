2a603b264561bdd2069518e8bc4b3a7687771514
q0_u_0p <q0_u_0p@naver.com>
2024-11-06 14:27:31 +0900

    🔨 Chore: modify the Path

--- chat-service/app/main.py
+++ chat-service/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from py_eureka_client import eureka_client
-from core.config import settings
-from api import chat_controller
+from .core.config import settings
+from .api import chat_controller
 
 import uvicorn
 import os
