6f49f496f41b97e1373203f129905d47bc3de872
kyungkeun <wlrudrms19@naver.com>
2024-11-07 09:45:02 +0900

    :white_check_mark: Test: 로그인 화면 전환 테스트

--- frontend/app/src/main/java/com/survivalcoding/a510/routers/AppRouter.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/routers/AppRouter.kt
@@ -16,6 +16,7 @@ import com.survivalcoding.a510.pages.ChatDetailPage
 import com.survivalcoding.a510.pages.ChatListPage
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
@@ -24,11 +25,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.survivalcoding.a510.R
+import com.survivalcoding.a510.pages.PermissionPage
 import com.survivalcoding.a510.pages.SettingPage
+import com.survivalcoding.a510.pages.SignUpCompletePage
+import com.survivalcoding.a510.pages.TermsAgreementPage
+import kotlinx.coroutines.delay
 
 
 object Routes {
     const val WELCOME_SCREEN = "welcomeScreen"
+    const val TERMS_AGREEMENT = "termsAgreement"
+    const val PERMISSION_PAGE = "permissionPage"
+    const val SIGNUP_COMPLETE = "signupComplete"
     const val CHAT_LIST = "chatListPage"
     const val CHAT_DETAIL = "chatDetailPage/{chatId}"
     const val SETTING_PAGE = "settingPage"
@@ -62,6 +70,35 @@ fun AppRouter(
             )
         }
 
+        // 약관 동의 페이지
+        composable(Routes.TERMS_AGREEMENT) {
+            TermsAgreementPage(
+                onNextClick = {
+                    navController.navigate(Routes.PERMISSION_PAGE)
+                }
+            )
+        }
+
+        // 권한 설정 페이지
+        composable(Routes.PERMISSION_PAGE) {
+            PermissionPage(
+                onNextClick = {
+                    navController.navigate(Routes.SIGNUP_COMPLETE)
+                }
+            )
+        }
+
+        // 가입 완료 페이지
+        composable(Routes.SIGNUP_COMPLETE) {
+            SignUpCompletePage()
+            LaunchedEffect(Unit) {
+                delay(2000)
+                navController.navigate(Routes.CHAT_LIST) {
+                    popUpTo(Routes.WELCOME_SCREEN) { inclusive = true }
+                }
+            }
+        }
+
         // 권한 설정 페이지
         composable(Routes.SETTING_PAGE) {
             SettingPage(navController)
@@ -82,6 +119,8 @@ fun AppRouter(
             val chatId = backStackEntry.arguments?.getInt("chatId") ?: return@composable
             ChatDetailPage(navController, chatId)
         }
+
+
     }
 }
 
