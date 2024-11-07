b2308ec299c2d69f24c2968095718b335387f14a
김인엽 <inyub98@gmail.com>
2024-11-07 16:20:58 +0900

    :sparkles: Feat: add exception for kakao api

---
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/exception/KakaoApiRequestException.java
@@ -0,0 +1,8 @@
+package com.heejuk.tuddyfuddy.contextservice.exception;
+
+public class KakaoApiRequestException extends RuntimeException {
+
+    public KakaoApiRequestException(String message) {
+        super(message);
+    }
+}
