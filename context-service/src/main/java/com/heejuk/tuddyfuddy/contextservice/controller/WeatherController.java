fc1fdccc46795be78434220a4051018c7f942a52
김인엽 <inyub98@gmail.com>
2024-11-11 10:49:52 +0900

    :sparkles: Feat: add user id 추출 logic

--- context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/controller/WeatherController.java
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/controller/WeatherController.java
@@ -5,13 +5,19 @@ import com.heejuk.tuddyfuddy.contextservice.dto.response.WeatherListResponse;
 import com.heejuk.tuddyfuddy.contextservice.dto.response.WeatherLocationResponse;
 import com.heejuk.tuddyfuddy.contextservice.service.LocationService;
 import com.heejuk.tuddyfuddy.contextservice.service.WeatherService;
+import com.heejuk.tuddyfuddy.contextservice.util.HeaderUtil;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/context/weather")
@@ -22,9 +28,12 @@ public class WeatherController {
 
     @GetMapping
     public CommonResponse<WeatherLocationResponse> getWeatherContextData(
+        @RequestHeader HttpHeaders headers,
         @RequestParam("latitude") String latitude,
         @RequestParam("longitude") String longitude
     ) {
+        String userId = HeaderUtil.getUserHeaderInfo(headers);
+        log.info("userId = " + userId);
         WeatherListResponse weathersByLocation = weatherService.getWeathersByLocation(latitude,
                                                                                       longitude);
         String location = locationService.getLocation(longitude, latitude);
@@ -38,11 +47,13 @@ public class WeatherController {
     }
 
     @PostMapping("/async")
+    @Hidden
     public void asdf1() {
         weatherService.dailyFetchAll();
     }
 
     @PostMapping("/sync")
+    @Hidden
     public void asdf2() {
         weatherService.dailyFetchAllSequential();
     }
