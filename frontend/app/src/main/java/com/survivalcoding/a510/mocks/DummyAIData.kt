260fed0b915fbb0d9843d8454d21a585a92e142e
최영빈 <swybean@gmail.com>
2024-11-12 14:27:23 +0900

    :bug: Fix: 이미지 형식 오류 수정

--- frontend/app/src/main/java/com/survivalcoding/a510/mocks/DummyAIData.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/mocks/DummyAIData.kt
@@ -39,7 +39,7 @@ object DummyAIData {
         ),
         ChatData(
             id = 5,
-            profileImage = R.drawable.group_ro,
+            profileImage = R.drawable.group_profile,
             name = "햇님이, 달님이",
             message = "단톡방에서 여러 친구들과 함께 대화를 시작해보세요!",
             timestamp = "방금 전",
