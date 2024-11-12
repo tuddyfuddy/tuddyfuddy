4fcdb6a3f56e5b8977d6d886773d1c33f5988a89
최영빈 <swybean@gmail.com>
2024-11-12 13:59:46 +0900

    :recycle: Refactor: AIData 수정

--- frontend/app/src/main/java/com/survivalcoding/a510/mocks/DummyAIData.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/mocks/DummyAIData.kt
@@ -7,40 +7,40 @@ object DummyAIData {
     val chatList = listOf(
         ChatData(
             id = 1,
-            profileImage = R.drawable.cha,
-            name = "차은우",
-            message = "야 대답해",
+            profileImage = R.drawable.tuddy,
+            name = "Tuddy",
+            message = "야 뭐하고 있냐",
             timestamp = "방금 전",
             unreadCount = 1
         ),
         ChatData(
             id = 2,
-            profileImage = R.drawable.back,
-            name = "백지헌",
+            profileImage = R.drawable.fuddy,
+            name = "Fuddy",
             message = "하이하이!! 너는 이름이 뭐야?",
             timestamp = "방금 전",
             unreadCount = 1
         ),
         ChatData(
             id = 3,
-            profileImage = R.drawable.kim,
-            name = "김유정",
-            message = "심심하면 나랑 수다나 떨자",
+            profileImage = R.drawable.otter,
+            name = "달님이",
+            message = "만나서 반가워, 너는 이름이 뭐야?",
             timestamp = "방금 전",
             unreadCount = 1
         ),
         ChatData(
             id = 4,
-            profileImage = R.drawable.karina,
-            name = "카리나",
-            message = "이번 주말에 뭐해?",
+            profileImage = R.drawable.rabbit,
+            name = "햇님이",
+            message = "방가방가!! 나는 햇님이야! 너는 이름이 뭐야~?",
             timestamp = "방금 전",
             unreadCount = 1
         ),
         ChatData(
             id = 5,
-            profileImage = R.drawable.grouptalk_white,
-            name = "김유정, 카리나",
+            profileImage = R.drawable.group_ro,
+            name = "햇님이, 달님이",
             message = "단톡방에서 여러 친구들과 함께 대화를 시작해보세요!",
             timestamp = "방금 전",
             unreadCount = 1
