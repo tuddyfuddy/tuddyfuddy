82aa1b05bf565df17a96d0cc83603d4150f9f9c3
김인엽 <inyub98@gmail.com>
2024-11-13 13:48:31 +0900

    :sparkles: Feat: change logic for exporting header token

--- context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/util/HeaderUtil.java
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/util/HeaderUtil.java
@@ -2,14 +2,20 @@ package com.heejuk.tuddyfuddy.contextservice.util;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 
+@Slf4j
 public class HeaderUtil {
 
     public static String getUserHeaderInfo(HttpHeaders headers) {
-
-        return headers.get("X-UserId")
-                      .get(0);
+        String userId = null;
+        if (headers.get("X-UserId") != null) {
+            userId = headers.get("X-UserId")
+                            .get(0);
+        }
+        log.info("userId = {}", userId);
+        return userId;
     }
 
     public static String formatWithCommas(long amount) {
