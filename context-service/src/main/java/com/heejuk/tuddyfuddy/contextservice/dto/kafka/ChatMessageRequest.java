7e01b61ed78ee3b01a6c5d10dcbfd10c93677ff9
김인엽 <inyub98@gmail.com>
2024-11-14 15:35:16 +0900

    :sparkles: Feat: add chat message request dto for kafka to notification-service

---
+++ context-service/src/main/java/com/heejuk/tuddyfuddy/contextservice/dto/kafka/ChatMessageRequest.java
@@ -0,0 +1,37 @@
+package com.heejuk.tuddyfuddy.contextservice.dto.kafka;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+public record ChatMessageRequest(
+
+    @Schema(description = "User ID")
+    String userId,
+
+    @Schema(description = "Room ID")
+    Integer roomId,
+
+    @Schema(description = "AI Name")
+    String aiName,
+
+    @Schema(description = "Chat message content")
+    String message
+
+) {
+
+    @JsonCreator
+    public ChatMessageRequest(
+        @JsonProperty("userId") String userId,
+        @JsonProperty("roomId") Integer roomId,
+        @JsonProperty("aiName") String aiName,
+        @JsonProperty("message") String message
+    ) {
+        this.userId = userId;
+        this.roomId = roomId;
+        this.aiName = aiName;
+        this.message = message;
+    }
+}
