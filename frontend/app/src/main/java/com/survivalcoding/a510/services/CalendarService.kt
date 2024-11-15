00c1e49fd7f36fa484663dd31f6f89e748ce9efd
최영빈 <swybean@gmail.com>
2024-11-15 13:26:57 +0900

    :sparkles: Feat: 날씨&일정 알림 내용 RoomDB 저장

--- frontend/app/src/main/java/com/survivalcoding/a510/services/CalendarService.kt
+++ frontend/app/src/main/java/com/survivalcoding/a510/services/CalendarService.kt
@@ -3,10 +3,11 @@ package com.survivalcoding.a510.services
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface CalendarService {
-    @GET("/context/calendar")
+    @POST("/context/calendar")
     fun sendCalendarTitle(
         @Query("title") title: String
     ): Call<ResponseBody>
