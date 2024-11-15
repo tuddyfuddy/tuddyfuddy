5fc511ae5501ff5d73128ad933f26f5c5ade0527
김인엽 <inyub98@gmail.com>
2024-11-15 15:24:57 +0900

    :sparkles: Feat: update url

--- image-service/src/main/java/com/heejuk/tuddyfuddy/imageservice/client/ImageCreateAiClient.java
+++ image-service/src/main/java/com/heejuk/tuddyfuddy/imageservice/client/ImageCreateAiClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "image-create-ai-client", url = "http://70.12.130.131:5100/", configuration = FeignConfig.class)
+@FeignClient(name = "image-create-ai-client", url = "http://222.107.238.75:5100/", configuration = FeignConfig.class)
 public interface ImageCreateAiClient {
 
     @PostMapping
