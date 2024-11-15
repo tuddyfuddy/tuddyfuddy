082c14f8d68abca8ea4d229092621c8b7aa1a1a4
김인엽 <inyub98@gmail.com>
2024-11-15 12:24:47 +0900

    :sparkles: Feat: add endpoints for image generate

--- image-service/src/main/java/com/heejuk/tuddyfuddy/imageservice/controller/ImageController.java
+++ image-service/src/main/java/com/heejuk/tuddyfuddy/imageservice/controller/ImageController.java
@@ -1,9 +1,10 @@
 package com.heejuk.tuddyfuddy.imageservice.controller;
 
+import com.heejuk.tuddyfuddy.imageservice.client.ImageCreateAiClient;
 import com.heejuk.tuddyfuddy.imageservice.dto.CommonResponse;
+import com.heejuk.tuddyfuddy.imageservice.dto.request.ImageCreateRequest;
 import com.heejuk.tuddyfuddy.imageservice.dto.response.ImageAnalysisResponse;
-import com.heejuk.tuddyfuddy.imageservice.service.ImageAnalysisService;
-import com.heejuk.tuddyfuddy.imageservice.service.ImagePostService;
+import com.heejuk.tuddyfuddy.imageservice.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.concurrent.CompletableFuture;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,18 +22,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/images/analysis")
-@Tag(name = "Image Analysis", description = "이미지 분석 API")
+@RequestMapping("/images")
+@Tag(name = "Image Analysis", description = "이미지 API")
 public class ImageController {
 
-    private final ImageAnalysisService imageAnalysisService;
-    private final ImagePostService imagePostService;
+    private final ImageService imageService;
+    private final ImageCreateAiClient aiClient;
 
     @Operation(
         summary = "이미지 분석",
         description = "이미지를 받아 GPT-4 로 분석합니다."
     )
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/analysis", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public CommonResponse<ImageAnalysisResponse> analyzeImageWithChatbot(
         @RequestPart("image") MultipartFile image
     ) {
@@ -42,7 +44,7 @@ public class ImageController {
             CompletableFuture<String> descriptionFuture = CompletableFuture
                 .supplyAsync(() -> {
                     long analysisStartTime = System.currentTimeMillis();
-                    String result = imageAnalysisService.analyzeImage(image);
+                    String result = imageService.analyzeImage(image);
                     long analysisEndTime = System.currentTimeMillis();
                     log.info("이미지 분석 소요시간: {}초", (analysisEndTime - analysisStartTime) / 1000.0);
                     return result;
@@ -51,7 +53,7 @@ public class ImageController {
             CompletableFuture<String> imageUrlFuture = CompletableFuture
                 .supplyAsync(() -> {
                     long uploadStartTime = System.currentTimeMillis();
-                    String result = imagePostService.postImage(image);
+                    String result = imageService.postImage(image);
                     long uploadEndTime = System.currentTimeMillis();
                     log.info("이미지 업로드 소요시간: {}초", (uploadEndTime - uploadStartTime) / 1000.0);
                     return result;
@@ -79,5 +81,41 @@ public class ImageController {
                   .interrupt();
             throw new RuntimeException("이미지 처리 중 오류가 발생했습니다.", e);
         }
+    }
+
+    @Operation(
+        summary = "이미지 생성",
+        description = "GPT-4 에게 프롬프트를 넣어서 이미지를 생성해 S3에 넣고 해당 이미지 URL 을 반환합니다."
+    )
+    @PostMapping
+    public CommonResponse<String> createImage(
+        @RequestBody ImageCreateRequest request
+    ) {
+        long startTime = System.currentTimeMillis();
+
+        String imageUrl = imageService.createImage(request.text());
+
+        long endTime = System.currentTimeMillis();
+        log.info("전체 처리 소요시간: {}초", (endTime - startTime) / 1000.0);
+
+        return CommonResponse.ok("이미지 생성 완료", imageUrl);
+    }
+
+    @Operation(
+        summary = "이미지 생성 (갈덕이형꺼 API)",
+        description = "???에게 프롬프트를 넣어서 이미지를 생성해 S3에 넣고 해당 이미지 URL 을 반환합니다."
+    )
+    @PostMapping("/test")
+    public CommonResponse<String> createImage2(
+        @RequestBody ImageCreateRequest request
+    ) {
+        long startTime = System.currentTimeMillis();
+
+        Object imageUrl = aiClient.createImage(request);
+
+        long endTime = System.currentTimeMillis();
+        log.info("전체 처리 소요시간: {}초", (endTime - startTime) / 1000.0);
+        log.info("결과값 : {}", imageUrl.toString());
+        return CommonResponse.ok("이미지 생성 완료");
     }
 }
